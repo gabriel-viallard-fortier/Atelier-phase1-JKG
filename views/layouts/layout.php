55e3869492547e2a1d8edf4632749235fc88d525
Fatima El mouhine <fatima.elmouhine@laplateforme.io>
2025-08-05 10:40:05 +0200

    Add 'Forgot Password' link to the authentication navigation in layout

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -22,6 +22,7 @@
                 <?php else: ?>
                     <li><a href="<?php echo url('auth/login'); ?>">Connexion</a></li>
                     <li><a href="<?php echo url('auth/register'); ?>">Inscription</a></li>
+                    <li><a href="<?php echo url('auth/forgot-password'); ?>">Mot de passe oublié</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
