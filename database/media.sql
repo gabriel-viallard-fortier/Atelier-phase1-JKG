4724031c631fe53533271a726a3e9c9c62c283e9
johan <johan.chevet@laplateforme.io>
2025-08-28 09:29:59 +0200

    fix syntax errors

--- database/media.sql
+++ database/media.sql
@@ -2,14 +2,14 @@ USE php_mvc_app;
 
 CREATE TABLE IF NOT EXISTS medias (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    type ENUM('Book', 'Movie', 'Game') NOT NULL,
+    type ENUM('Book', 'Movie', 'Game') NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS books (
     id INT NOT NULL,
     title VARCHAR(200) NOT NULL CHECK (LENGTH(title) > 0 AND LENGTH(title) <= 200),
     genre ENUM('Action', 'Comedy', 'Documentary', 'Drama', 'Fantasy', 'Horror', 'Musical', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'Western') NOT NULL,
-    stock INT NOT NULL
+    stock INT NOT NULL,
     author VARCHAR(100) NOT NULL CHECK (LENGTH(author) > 2 AND LENGTH(author) <= 100),
     isbn VARCHAR(13) NOT NULL UNIQUE CHECK (LENGTH(isbn) = 10 OR LENGTH(isbn) = 13),
     pages INT NOT NULL CHECK (pages > 1 AND pages <= 9999),
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS movies (
     id INT NOT NULL,
     title VARCHAR(200) NOT NULL CHECK (LENGTH(title) > 0 AND LENGTH(title) <= 200),
     genre ENUM('Action', 'Comedy', 'Documentary', 'Drama', 'Fantasy', 'Horror', 'Musical', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'Western') NOT NULL,
-    stock INT NOT NULL
+    stock INT NOT NULL,
     director VARCHAR(100) NOT NULL CHECK (LENGTH(director) > 2 AND LENGTH(director) <= 100),
     duration INT NOT NULL CHECK (duration > 0 AND duration <= 999),
     published_year INT NOT NULL CHECK (published_year >= 1900),
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS movies (
 CREATE TABLE IF NOT EXISTS games (
     id INT NOT NULL,
     title VARCHAR(200) NOT NULL CHECK (LENGTH(title) > 0 AND LENGTH(title) <= 200),
-    genre ENUM('FPS', 'MMO', 'MOBA' 'RPG'),
-    stock INT NOT NULL
+    genre ENUM('FPS', 'MMO', 'MOBA', 'RPG'),
+    stock INT NOT NULL,
     editor VARCHAR(100) NOT NULL CHECK (LENGTH(editor) > 2 AND LENGTH(editor) <= 100),
     plateform ENUM('PC', 'PlayStation', 'Xbox', 'Nintendo', 'Mobile') NOT NULL,
     pegi ENUM('3', '7', '12', '16', '18') NOT NULL,
